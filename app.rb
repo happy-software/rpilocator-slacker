dc3fd5efdc65941d6932e582e6cf0dd9a5a1985c
Hebron George <hebrontgeorge@gmail.com>
2023-02-21 21:32:07 -0500

    Remove hard coded test values

--- app.rb
+++ app.rb
@@ -5,22 +5,6 @@ require_relative 'slack_notifier'
 class App
   def run
     available_pis = RpilocatorParser.new.get_available
-    available_pis =  [{"update_t"=>{"sort"=>2, "display"=>"2 min."},
-                      "price"=>{"sort"=>75, "display"=>"75.00", "currency"=>"USD"},
-                      "vendor"=>"Chicago Elec. Dist. (US)",
-                      "sku"=>"RPI4-MODBP-8GB",
-                      "avail"=>"No",
-                      "link"=>"https://chicagodist.com/products/raspberry-pi-4-model-b-8gb",
-                      "last_stock"=>{"sort"=>"", "display"=>""},
-                      "description"=>"RPi 4 Model B - 8GB RAM"},
-      {"update_t"=>{"sort"=>0, "display"=>"0 min."},
-       "price"=>{"sort"=>75, "display"=>"75.00", "currency"=>"USD"},
-       "vendor"=>"Pishop (US)",
-       "sku"=>"RPI4-MODBP-8GB",
-       "avail"=>"No",
-       "link"=>"https://www.pishop.us/product/raspberry-pi-4-model-b-8gb/",
-       "last_stock"=>{"sort"=>"", "display"=>""},
-       "description"=>"RPi 4 Model B - 8GB RAM"}]
     if available_pis.any?
       message = format_message(available_pis)
       SlackNotifier.new.post_message(message, ENV['NOTIFICATION_CHANNEL'])
